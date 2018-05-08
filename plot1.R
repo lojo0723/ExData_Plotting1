b9f9ddcb29a0c26a6e40b6b5d7cf2b08ea9534b4
Logan Johnson <32250394+lojo0723@users.noreply.github.com>
2018-05-08 09:42:18 -0400

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,9 @@
+
+EPC <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+names(EPC) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subEPC <- subset(EPC,EPC$Date=="1/2/2007" | EPC$Date =="2/2/2007")
+
+hist(as.numeric(as.character(subEPC$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+
+dev.copy(png, file = "plot1.png", width=480, height=480)
+dev.off()
